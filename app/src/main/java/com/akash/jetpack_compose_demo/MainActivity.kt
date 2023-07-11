c57568fa4c07cdc833149d84a3062ac5f678df73
Sabir Khan Akash <sabir.akash.cs@gmail.com>
2023-07-12 02:49:24 +0600

    header, footer and logo added

--- app/src/main/java/com/akash/jetpack_compose_demo/MainActivity.kt
+++ app/src/main/java/com/akash/jetpack_compose_demo/MainActivity.kt
@@ -3,13 +3,30 @@ package com.akash.jetpack_compose_demo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.core.content.res.TypedArrayUtils.getString
 import androidx.core.content.res.TypedArrayUtils.getText
 import com.akash.jetpack_compose_demo.ui.theme.JetpackcomposedemoTheme
@@ -25,7 +42,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting()
+                    app()
                 }
             }
         }
@@ -33,17 +50,58 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting() {
-    var name: String? = get(R.string.app_name.toString())
-    Text(
-        text = "Hello " + name
-    )
+fun app() {
+    /**
+     *Language Changer
+     */
+    Column(
+        modifier = Modifier.padding(all = 20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "English (US)"
+        )
+    }
+    /**
+     *App Logo
+     */
+    Column(
+        modifier = Modifier.padding(120.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.app_logo),
+            contentDescription = "app logo",
+            colorFilter = ColorFilter.tint(color = Color.Black),
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .clip(RoundedCornerShape(50.dp))
+        )
+    }
+    /**
+     * Username EditText
+     */
+
+    /**
+     *App Footer
+     */
+    Column(
+        verticalArrangement = Arrangement.Bottom,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(painter = painterResource(id = R.drawable.meta_footer),
+            contentDescription = "footer",
+            contentScale = ContentScale.Fit,
+            modifier = Modifier
+                .size(80.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun GreetingPreview() {
+fun appPreview() {
     JetpackcomposedemoTheme {
-        Greeting()
+        app()
     }
 }
