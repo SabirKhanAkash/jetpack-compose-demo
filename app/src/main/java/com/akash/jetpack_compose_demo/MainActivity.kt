5fff1517b5e900f39f0f4cbc81f2cc9ce82295b2
Sabir Khan Akash <sabir.akash.cs@gmail.com>
2023-07-13 11:56:27 +0600

    ui update
    - Employee ID and password TextField added,
    - Login Button added

--- app/src/main/java/com/akash/jetpack_compose_demo/MainActivity.kt
+++ app/src/main/java/com/akash/jetpack_compose_demo/MainActivity.kt
@@ -5,42 +5,56 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.core.content.res.TypedArrayUtils.getString
-import androidx.core.content.res.TypedArrayUtils.getText
 import com.akash.jetpack_compose_demo.ui.theme.JetpackcomposedemoTheme
-import org.w3c.dom.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackcomposedemoTheme {
+            MaterialTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
                 ) {
                     app()
                 }
@@ -49,50 +63,149 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun app() {
     /**
+     * Background Image
+     */
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.login_background),
+            contentDescription = "background",
+            contentScale = ContentScale.FillBounds,
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+        )
+    }
+    /**
      *Language Changer
      */
-    Column(
-        modifier = Modifier.padding(all = 20.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
+    Row(
+        modifier = Modifier
+            .padding(all = 20.dp)
+            .fillMaxWidth()
     ) {
         Text(
-            text = "English (US)"
+            text = "English (US)",
+            color = Color.White,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentSize(Alignment.Center)
         )
     }
     /**
      *App Logo
      */
-    Column(
-        modifier = Modifier.padding(120.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
+    Row(
+        modifier = Modifier
+            .wrapContentSize(Alignment.TopCenter)
+            .padding(top = 120.dp)
     ) {
         Image(
-            painter = painterResource(id = R.drawable.app_logo),
+            painter = painterResource(id = R.drawable.login_logo),
             contentDescription = "app logo",
-            colorFilter = ColorFilter.tint(color = Color.Black),
             modifier = Modifier
-                .size(100.dp)
+                .size(150.dp)
+                .fillMaxWidth()
+                .wrapContentSize(Alignment.Center)
         )
     }
     /**
-     * Username EditText
+     * Username TextField
      */
-
+    Row(
+        modifier = Modifier
+            .padding(20.dp, 300.dp, 20.dp, 0.dp)
+            .fillMaxWidth(),
+    ) {
+        var username by rememberSaveable {
+            mutableStateOf("")
+        }
+        OutlinedTextField(
+            value = username,
+            onValueChange = { username = it },
+            textStyle = TextStyle(color = Color.White),
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                focusedBorderColor = Color.White.copy(alpha = 0.8f),
+                unfocusedLabelColor = Color.White.copy(alpha = 0.8f),
+                focusedLabelColor = Color.White.copy(alpha = 0.8f),
+                cursorColor = Color.White
+            ),
+            label = { Text("Employee ID")},
+            modifier = Modifier
+                .fillMaxWidth()
+        )
+    }
+    /**
+     * Password TextField
+     */
+    Row(
+        modifier = Modifier.padding(20.dp,380.dp,20.dp,0.dp),
+    ) {
+        var password by rememberSaveable {
+            mutableStateOf("")
+        }
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Password") },
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                focusedBorderColor = Color.White.copy(alpha = 0.8f),
+                unfocusedLabelColor = Color.White.copy(alpha = 0.8f),
+                focusedLabelColor = Color.White.copy(alpha = 0.8f),
+                cursorColor = Color.White
+            ),
+            visualTransformation = PasswordVisualTransformation(),
+            textStyle = TextStyle(color = Color.White),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+    /**
+     * Login Button
+     */
+    Row(
+        modifier = Modifier
+            .padding(top = 470.dp)
+            .wrapContentSize(Alignment.TopCenter)
+            .fillMaxWidth()
+    ) {
+        Button(
+            modifier = Modifier
+                .padding(start = 20.dp, end = 20.dp)
+                .fillMaxWidth()
+                .height(45.dp),
+            onClick = { /*TODO*/ },
+            shape = RoundedCornerShape(size = 8.dp),
+            colors = ButtonDefaults.buttonColors(
+                contentColor = Color.White,
+                containerColor = Color.Blue
+            )
+        ) {
+            Text(text = "LOG IN")
+        }
+    }
     /**
      *App Footer
      */
-    Column(
-        verticalArrangement = Arrangement.Bottom,
-        horizontalAlignment = Alignment.CenterHorizontally
+    Row(
+        modifier = Modifier
+            .padding(bottom = 10.dp)
+            .wrapContentSize(Alignment.BottomCenter)
     ) {
-        Image(painter = painterResource(id = R.drawable.meta_footer),
+        Image(
+            painter = painterResource(id = R.drawable.login_footer),
             contentDescription = "footer",
             contentScale = ContentScale.Fit,
             modifier = Modifier
                 .size(80.dp)
+                .fillMaxWidth()
         )
     }
 }
@@ -100,7 +213,7 @@ fun app() {
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun appPreview() {
-    JetpackcomposedemoTheme {
+    MaterialTheme {
         app()
     }
 }
