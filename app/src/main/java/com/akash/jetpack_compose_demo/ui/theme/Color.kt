51d1e79b1d342ca732925de5c55a6f158eec1f95
Sabir Khan Akash <39434260+SabirKhanAkash@users.noreply.github.com>
2024-02-27 21:29:27 +0600

    cleaned all

--- app/src/main/java/com/akash/jetpack_compose_demo/ui/theme/Color.kt
+++ app/src/main/java/com/akash/jetpack_compose_demo/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.akash.jetpack_compose_demo.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFFFFFFF)
-val PurpleGrey80 = Color(0xFFB6B6B6)
-val Pink80 = Color(0xFF535353)
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF000000)
-val PurpleGrey40 = Color(0xFF424142)
-val Pink40 = Color(0xFFC2C2C2)
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
